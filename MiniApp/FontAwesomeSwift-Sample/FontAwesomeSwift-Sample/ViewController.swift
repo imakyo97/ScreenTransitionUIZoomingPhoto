8af4d01e2cc7c06159e0883a9dd1e7f703cfd9bc
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-12-22 10:26:00 +0900

    その他の部品でも実装

--- MiniApp/FontAwesomeSwift-Sample/FontAwesomeSwift-Sample/ViewController.swift
+++ MiniApp/FontAwesomeSwift-Sample/FontAwesomeSwift-Sample/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet private weak var label2: UILabel!
     @IBOutlet private weak var label3: UILabel!
     @IBOutlet private weak var label4: UILabel!
+    @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var barButton: UIBarButtonItem!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var imageViewColored: UIImageView!
 
     private lazy var labels: [UILabel] = [label1, label2, label3, label4]
     private let styles: [FontAwesomeStyle] = [.solid, .light, .regular, .brands]
@@ -21,6 +25,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLabel()
+        setupButton()
+        setupBarButton()
+        setupImageView()
     }
 
     private func setupLabel() {
@@ -28,6 +35,23 @@ class ViewController: UIViewController {
             label.font = UIFont.fontAwesome(ofSize: 50, style: style)
             label.text = String.fontAwesomeIcon(name: .school)
         }
+    }
+
+    private func setupButton() {
+        button.titleLabel?.font = UIFont.fontAwesome(ofSize: 50, style: .brands)
+        button.setTitle(String.fontAwesomeIcon(name: .git), for: .normal)
+    }
+
+    private func setupBarButton() {
+        let attributes = [NSAttributedString.Key.font: UIFont.fontAwesome(ofSize: 20, style: .brands)]
+        barButton.setTitleTextAttributes(attributes, for: .normal)
+        barButton.title = String.fontAwesomeIcon(name: .apple)
+    }
+
+    private func setupImageView() {
+        imageView.image = UIImage.fontAwesomeIcon(name: .applePay, style: .brands, textColor: .black, size: CGSize(width: 100, height: 50))
+
+        imageViewColored.image = UIImage.fontAwesomeIcon(name: .applePay, style: .brands, textColor: .white, size: CGSize(width: 100, height: 50), backgroundColor: .black)
     }
 }
 
